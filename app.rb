1ab91a579b223b2d3113c540cbc05daebdb27c53
Romario Gustave <romagustave10@gmail.com>
2020-07-13 02:18:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,8 +10,14 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
+<<<<<<< HEAD
   post '/puppy' do
     @puppy = Puppy.new(params[:name], params[:breed], params[:age])
+=======
+  post '/' do
+    @new_pooch = Puppy.new(params[:name], params[:breed], params[:age])
+
+>>>>>>> 00e73ba9f6f42a0cdb577a61026c86370849c9b2
     erb :display_puppy
   end
 end
